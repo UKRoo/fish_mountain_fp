25c100e66151227b63c1f483f36b59a148df3f85
UKRoo <ivashko.dmytro@gmail.com>
2020-01-13 20:03:26 +0200

    jq

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import 'mdbootstrap/js/jquery.js'
+// import '@/assets/js/jquery-migrate-3.1.0.min.js' //npm mig install 
+import '@/assets/js/slick.js'
 import 'mdbootstrap/js/popper.js'
 import 'mdbootstrap/js/bootstrap.js'
 import 'mdbootstrap/js/mdb.js'
