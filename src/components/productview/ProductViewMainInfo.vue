16a26b806bcba5c162eb3c6b998fff0f0f1596da
Dmytro Ivashko <ivashko.dmytro@gmail.com>
2020-01-23 18:29:19 +0200

    few fix

--- src/components/productview/ProductViewMainInfo.vue
+++ src/components/productview/ProductViewMainInfo.vue
@@ -7,9 +7,34 @@
     <h2>Man</h2>
     <div class="fs-prod-vw-info-rew flex-sb"><h2><b>$750</b></h2><div>rew</div></div>
     <div class="fs-prod-vw-info-style flex-sb"><h2>Fit</h2><div>rew</div></div>
-    <div class="fs-prod-vw-info-size flex-sb">
+    <div class="fs-prod-vw-info-size">
         <h3>Select a Size </h3>
-        
+        <div class="fs-size-block"> 
+            <div class="size-square">S</div>
+            <div class="size-square">M</div>
+            <div class="size-square">L</div>
+            <div class="size-square">XL</div>
+            <div class="size-square">XXL</div>
+        </div>
+        <h3><a>Size Guide</a></h3>
+    <div class="fs-prod-vw-info-color flex-sb">
+      <div class="fs-select-color">
+        <h3>Select a Color</h3>
+        <div class="color-block">
+        <div class="fs-select-color-1"></div>
+        <div class="fs-select-color-2"></div>
+        </div>
+        </div>
+      <div class="fs-select-qty">
+        <h3> Quantity</h3>
+        <select class="browser-default custom-select">
+  <option value="1" selected>1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+</select>
+        </div>
+      </div>
     </div>
   </div>
 </h2>
